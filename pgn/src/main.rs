d7199826bab53d4d629875a10b4c6010679dc113
Simon B <scb5436@rit.edu>
2023-09-11 22:48:21 -0400

    Why is it so difficult to find a file rust...

---
+++ pgn/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+    let mut inp: String = String::new();
+    std::io::stdin().read_line(&mut inp).unwrap();
+
+    println!(
+        "Trying to open file: {inp} from {:?}",
+        env::current_dir().unwrap()
+    );
+
+    let path = Path::new(&inp);
+
+    // let path3 = path::Path::new("wtf2.txt");
+    println!("{}", path.display());
+    let contents: String = fs::read_to_string(path).expect("Failed to open file");
+    println!("{:?}", contents);
+
+    Ok(())
+}
