8865ff44ae966ed5b9de4fa62ee76c0bcc13ce45
Simon B <scb5436@rit.edu>
2023-09-12 21:57:42 -0400

    cmd line args; learned map,collect,match,types,etc

--- pgn/src/main.rs
+++ pgn/src/main.rs
@@ -1,24 +1,25 @@
 use std::env;
 use std::fs;
-use std::path::Path;
 
 fn main() -> std::io::Result<()> {
-    let mut inp: String = String::new();
-    std::io::stdin().read_line(&mut inp).unwrap();
+    // Get args and check for possibility of a file path existing in argv[1]
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("No file provided!");
+        println!("Usage: pgn PGN_FILE");
+        std::process::exit(-1);
+    }
+    let pgn: &String = &args[1];
+    println!("Attempting to parse {}...", pgn);
 
-    let inp = inp.trim();
+    // Attempt to open file and then convert bytes to utf8 slice
+    let contents: Vec<u8> = fs::read(pgn).expect("Failed to open file");
+    let contents: &str = match std::str::from_utf8(&contents) {
+        Ok(s) => s,
+        Err(e) => panic!("Failed during utf-8 decoding {}", e),
+    };
 
-    println!(
-        "Trying to open file: {inp} from {:?}",
-        env::current_dir().unwrap()
-    );
-
-    let path = Path::new(&inp);
-
-    // let path3 = path::Path::new("wtf2.txt");
-    println!("{}", path.display());
-    let contents: String = fs::read_to_string(path).expect("Failed to open file");
-    println!("{:?}", contents);
+    println!("{contents}");
 
     Ok(())
 }
